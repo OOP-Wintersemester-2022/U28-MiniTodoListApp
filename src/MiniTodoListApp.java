cd15d0ceda7ba53c80e2b65d1fcf18d0ad243ca2
Jürgen Hahn <juergen.hahn@ur.de>
2021-12-21 12:00:35 +0100

    added starter

--- src/MiniTodoListApp.java
+++ src/MiniTodoListApp.java
@@ -1,32 +1,22 @@
 import de.ur.mi.oop.app.GraphicsApp;
-import de.ur.mi.oop.events.*;
+import de.ur.mi.oop.colors.Colors;
 import de.ur.mi.oop.launcher.GraphicsAppLauncher;
 
-public class MiniTodoListApp extends GraphicsApp implements GraphicsAppMouseListener, GraphicsAppKeyListener {
-    private TodoList todoList;
-    private TodoListView todoListView;
+public class MiniTodoListApp extends GraphicsApp {
+    private static final int APP_WIDTH = 280;
+    private static final int APP_HEIGHT = 360;
 
     @Override
     public void initialize() {
-        setCanvasSize(Config.APP_WIDTH, Config.APP_HEIGHT);
-
-        this.todoList = new TodoList();
-        this.todoListView = new TodoListView(todoList);
+        setCanvasSize(APP_WIDTH, APP_HEIGHT);
     }
 
     @Override
     public void draw() {
-        drawBackground(Config.APP_BACKGROUND_COLOR);
-        this.todoListView.draw();
+        drawBackground(Colors.WHITE);
     }
 
     public static void main(String[] args) {
         GraphicsAppLauncher.launch();
-    }
-
-    @Override
-    public void onMousePressed(MousePressedEvent event) {
-        super.onMousePressed(event);
-        this.todoListView.onClick(event.getXPos(), event.getYPos());
     }
 }
